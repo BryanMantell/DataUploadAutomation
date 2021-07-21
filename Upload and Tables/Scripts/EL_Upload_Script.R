cffdb832dec764d1a3ef3ccbc6b0ef01ea7959ed
KyleMMurray <60526752+KyleMMurray@users.noreply.github.com>
2021-07-21 15:58:52 -0700

    Update EL_Upload_Script.R

--- Upload and Tables/Scripts/EL_Upload_Script.R
+++ Upload and Tables/Scripts/EL_Upload_Script.R
@@ -1,33 +1,38 @@
 # Title: EL Upload Script
-
-#Setup
 #Setwd
 setwd("~/GitHub/DataUploadAutomation/Upload and Tables/Data")
 
 Emotion_Labeling_NDA <- read.csv("elt01_template.csv", skip=1)
 options(digits = 3)
 
-
-
 #EL_PREP <- select(Redcap_Data, c(child_guid, child_famID, interview_date, interview_age_child, child_sex, GroupAssignment, Timepoint, starts_with("oc_elt_")))
-EL_PREP <- select(Redcap_Data, c(child_guid, child_famID, interview_date, interview_age, child_sex, timepoint, starts_with("oc_elt_")))
-exp <- colnames(select(EL_PREP, c("oc_elt_exp_2", "oc_elt_exp_4", "oc_elt_exp_6", "oc_elt_exp_8")))
-
-rec <- colnames(select(EL_PREP, c("oc_elt_rec_1", "oc_elt_rec_2", "oc_elt_rec_3", "oc_elt_rec_4")))
+EL_PREP <- select(Redcap_Data, c(child_guid, child_famID, interview_date, interview_age_child, child_sex, Timepoint, starts_with("oc_elt_")))
 
 # add mean columns 
 EL_PREP$oc_elt_exp_total <- rowMeans(EL_PREP[,exp], na.rm = TRUE)
 EL_PREP$oc_elt_rec_total <- rowMeans(EL_PREP[,rec], na.rm = TRUE)
-
+
+# Remove -9999s
+EL_PREP[EL_PREP == -9999] <- NA
+
 # Move relevant info to NDA dataframe
-Emotion_Labeling_NDA <- select(EL_PREP, c(subjectkey = child_guid, src_subject_id = child_famID, interview_date, interview_age, sex = child_sex, visit = timepoint, starts_with("oc_elt_")))
+EL_NDA_Prep <- select(EL_PREP, c(subjectkey = child_guid, src_subject_id = child_famID, interview_date, interview_age = interview_age_child, sex = child_sex, visit = Timepoint, starts_with("oc_elt_")))
 
 # Recreate first line in orignial NDA file
-Emotion_Labeling_NDA <- bind_rows(Emotion_Labeling_NDA, EL_PREP)
+Emotion_Labeling_NDA <- bind_rows(mutate_all(Emotion_Labeling_NDA, as.character), mutate_all(EL_NDA_Prep, as.character))
 first_line <- matrix("", nrow = 1, ncol = ncol(Emotion_Labeling_NDA))
-first_line[,1] <- "el"
+
 # assign the second cell in first_line as "el"
+first_line[,1] <- "el"
 first_line[,2] <- "1"
+
+# NDA output ---------
+# Create a new file in folder called el.csv, and put first line into this file
+# el.csv file will be saved into same folder as current r script
+write.table(first_line, file = "NDA Upload/el01.csv", sep = ",", append = FALSE, quote = FALSE, na = "", col.names = FALSE, row.names = FALSE)
+
+# Append data in Emotion_Ladeling_NDA into el.csv file 
+write.table(Emotion_Labeling_NDA, file = 'NDA Upload/el01.csv', sep = ",", append = TRUE, na = "", quote = FALSE, row.names = FALSE)
 
 # NDA output ---------
 # Create a new file in folder called el.csv, and put first line into this file
