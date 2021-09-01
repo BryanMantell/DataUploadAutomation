348b30517353c5adcf3901dbd75cd6f7aad40c57
KyleMMurray <60526752+KyleMMurray@users.noreply.github.com>
2021-09-01 14:26:24 -0700

    fixed EL upload script

--- Upload and Tables/Scripts/EL_Upload_Script.R
+++ Upload and Tables/Scripts/EL_Upload_Script.R
@@ -6,17 +6,19 @@ Emotion_Labeling_NDA <- read.csv("elt01_template.csv", skip=1)
 options(digits = 3)
 
 #EL_PREP <- select(Redcap_Data, c(child_guid, child_famID, interview_date, interview_age_child, child_sex, GroupAssignment, Timepoint, starts_with("oc_elt_")))
-EL_PREP <- select(Redcap_Data, c(child_guid, child_famID, interview_date, interview_age_child, child_sex, Timepoint, starts_with("oc_elt_")))
+EL_PREP <- select(Redcap_Data, c(child_guid, child_famID, interview_date, interview_age_child, child_sex, Timepoint, starts_with("eltpart")))
 
 #Calculated Columns 
-EL_PREP <- add_column(EL_PREP, oc_elt_exp_total = varScore(EL_PREP, Forward = c("oc_elt_exp_2", "oc_elt_exp_4", "oc_elt_exp_6", "oc_elt_exp_8"), MaxMiss = .20),.after = "oc_elt_notes")
-EL_PREP <- add_column(EL_PREP, oc_elt_rec_total = varScore(EL_PREP, Forward = c("oc_elt_rec_1", "oc_elt_rec_2", "oc_elt_rec_3", "oc_elt_rec_4"), MaxMiss = .20),.after = "oc_elt_exp_total")
+#EL_PREP <- add_column(EL_PREP, oc_elt_exp_total = varScore(EL_PREP, Forward = c("oc_elt_exp_2", "oc_elt_exp_4", "oc_elt_exp_6", "oc_elt_exp_8"), MaxMiss = .20),.after = "oc_elt_notes")
+#EL_PREP <- add_column(EL_PREP, oc_elt_rec_total = varScore(EL_PREP, Forward = c("oc_elt_rec_1", "oc_elt_rec_2", "oc_elt_rec_3", "oc_elt_rec_4"), MaxMiss = .20),.after = "oc_elt_exp_total")
+EL_PREP <- add_column(EL_PREP, oc_elt_exp_total = varScore(EL_PREP, Forward = c("eltpart1_exp2", "eltpart1_exp4", "eltpart1_exp6", "eltpart1_exp8"), MaxMiss = .20),.after = "Timepoint")
+EL_PREP <- add_column(EL_PREP, oc_elt_rec_total = varScore(EL_PREP, Forward = c("eltpart2_rec1", "eltpart2_rec2", "eltpart2_rec3", "eltpart2_rec4"), MaxMiss = .20),.after = "oc_elt_exp_total")
 
 # Remove -9999s
 EL_PREP[EL_PREP == -9999] <- NA
 
 # Move relevant info to NDA dataframe
-EL_NDA_Prep <- select(EL_PREP, c(subjectkey = child_guid, src_subject_id = child_famID, interview_date, interview_age = interview_age_child, sex = child_sex, visit = Timepoint, starts_with("oc_elt_")))
+EL_NDA_Prep <- select(EL_PREP, c(subjectkey = child_guid, src_subject_id = child_famID, interview_date, interview_age = interview_age_child, sex = child_sex, visit = Timepoint, starts_with("eltpart")))
 
 #Remove unnecessary columns
 EL_NDA_Prep$oc_elt_notes <- NULL
